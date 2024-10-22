1651c363329cde637f622c0d1bb9bb4126ba5aa6
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-22 18:41:10 +0100

    isprint

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 11:57:50 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/22 12:03:16 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/10/22 15:30:42 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,4 @@
 int	ft_isprint(int c)
 {
 	return (c >= 32 && c <= 126);
-}
-
-int main(int argc,char **argv){
-	int i = 1;
-	while (i < argc)
-	{
-		int c = ft_isprint(argv[i][0]) + 48;
-		write(1, &c, 1);
-		i++;
-	}
-    return 0;
 }
