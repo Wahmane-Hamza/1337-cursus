9a7ad7c6040c7203cbbfcbd85ba1905a784ebd45
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-23 15:01:04 +0100

    m

--- ft_memset.c
+++ ft_memset.c
@@ -6,30 +6,28 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:58:50 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/22 18:40:15 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/10/23 10:53:24 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
 
 void	*ft_memset(void *str, int c, size_t n)
 {
-	unsigned char	*ptr;
+	char			*ptr;
+	unsigned int	uc;
 	size_t			i;
 
-	ptr = (unsigned char *)str;
+	ptr = str;
+	uc = c;
 	i = 0;
 	while (i < n)
-	{
-		ptr[i] = c;
-		i++;
-	}
+		ptr[i++] = uc;
 	return (str);
 }
 
 // int main()
 // {
 //     char str[50]="62ello world";
-//     printf("%s",ft_memset(str,'#',1));
+//     printf("%s",ft_memset(str,'#',5));
 // }
