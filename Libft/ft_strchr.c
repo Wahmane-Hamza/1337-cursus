7d2761eaf9f19741811902373c7b84dd2afb82af
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-25 20:07:04 +0100

    move

---
+++ Libft/ft_strchr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 16:26:05 by hwahmane          #+#    #+#             */
+/*   Updated: 2024/10/25 15:04:44 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	c2;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	c2 = c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c2)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+// int main() {
+// 	printf("%s\n",ft_strchr("Hello world",'\0'));
+// 	return 0;
+// }
