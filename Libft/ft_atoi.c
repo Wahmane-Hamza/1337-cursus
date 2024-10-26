9d6a24a61e217b55762adc332218fc8d887b4065
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-26 16:40:20 +0100

    atoi

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 13:35:16 by hwahmane          #+#    #+#             */
+/*   Updated: 2024/10/26 16:38:49 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_atoi(const char *str)
+{
+    int i;
+    long long k;
+    int sign;
+    
+    i = 0;
+    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    sign = 1;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign = sign * -1;
+        i++;
+    }
+    k = 0;
+    while (str[i] && str[i] >= '0' && str[i] <= '9')
+    {
+        k = (k * 10) +(str[i] - 48);
+        i++;
+    }
+    return (k * sign);
+}
+
+int main(int ac,char **av)
+{
+    int i = 1;
+    
+    while (i < ac)
+    {
+        // printf("%s\n",av[i]);
+        printf("%d\n",ft_atoi(av[i]));
+        printf("%d\n",atoi(av[i]));
+        i++;
+    }
+}
