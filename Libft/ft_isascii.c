69423c2cc665530680d4f96c74e06fb6ac01f829
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-27 19:04:59 +0100

    a

--- Libft/ft_isascii.c
+++ Libft/ft_isascii.c
@@ -6,11 +6,11 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:13:06 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/22 12:03:00 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/10/27 14:01:30 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libc.h"
 
 int	ft_isascii(int c)
 {
