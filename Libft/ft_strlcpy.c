2113e6e0d0e4920f3f23ed2fee28d5f439e7a686
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-27 11:26:44 +0100

    strc

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 07:27:43 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/24 14:44:22 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/10/27 11:23:49 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,24 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	j = 0;
 	while (src[j])
 		j++;
-	if (dst[0] == '\0')
-		return (0);
+	if (dstsize == 0)
+		return (j);
 	i = 0;
-	while (i < dstsize - 1)
+	while (i < dstsize - 1 && src[i])
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	dst[i] = 0;
+	dst[i] = '\0';
 	return (j);
 }
 
 // int main()
 // {
 // 	char src[] = "hello";
-// 	char dst[10] = "k";
+// 	char dst[10] = "";
 // 	printf("before :%s \n",dst);
-// 	ft_strlcpy(dst,src,sizeof(dst));
+// 	strlcpy(dst,src,sizeof(dst));
 // 	printf("after :%s \n",dst);
 
 // }
