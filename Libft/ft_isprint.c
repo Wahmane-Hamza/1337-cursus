116a83da1b6f4b23ba53cecc96d44d4a65651fd1
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-27 19:05:04 +0100

    a

--- Libft/ft_isprint.c
+++ Libft/ft_isprint.c
@@ -6,11 +6,11 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 11:57:50 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/22 15:30:42 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/10/27 14:01:35 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libc.h"
 
 int	ft_isprint(int c)
 {
