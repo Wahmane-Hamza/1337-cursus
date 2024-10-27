f9f5a3c9a7f934eaa10522b246f585be2dd996bd
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-27 19:05:33 +0100

    a

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,11 +6,23 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:45:12 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/24 15:31:42 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/10/27 18:42:51 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "libc.h"
+
+static size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -20,12 +32,10 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	if (!src || !dst || dstsize == 0)
 		return (0);
-	j = 0;
-	while (src[j])
-		j++;
-	k = 0;
-	while (dst[k])
-		k++;
+	j = ft_strlen(src);
+	k = ft_strlen(dst);
+	if (dstsize <= k)
+		return (k + j);
 	i = 0;
 	while (i + k < dstsize - 1)
 	{
@@ -33,15 +43,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		i++;
 	}
 	dst[k + i] = 0;
-	return (j);
+	return (k + j);
 }
 
 // int main()
 // {
 // 	char src[] = "hello";
-// 	char dst[10] = "";
+// 	char dst[] = "123456789123459";
 // 	printf("before :%s \n",dst);
-// 	ft_strlcat(dst,src,sizeof(dst));
+// 	ft_strlcat(dst,src,3);
 // 	printf("after :%s \n",dst);
 
 // }
