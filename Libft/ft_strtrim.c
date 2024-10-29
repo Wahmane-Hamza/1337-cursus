e17d7126f242cb7a85ace84208dc2f10b1605c49
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-29 07:21:33 +0100

    st

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,54 +6,50 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 09:25:03 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/28 19:18:52 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/10/29 07:21:16 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int is_char_in_set(char c,const char *set2)
+static int	is_char_in_set(char c, const char *set2)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(set2[i])
-    {
-        if (set2[i] == c)
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (set2[i])
+	{
+		if (set2[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    char    *ptr;
-    int     start;
-    int     end;
-    int     len;
+	char	*ptr;
+	int		start;
+	int		end;
+	int		len;
 
-    if (!s1, !set)
-        return (NULL);
-    start = 0;
-    end = ft_strlen(s1) - 1;
-    while (s1[start] && is_char_in_set(s1[start],set))
-        start++;
-    while (end >= start && is_char_in_set(s1[end],set))
-        end--;
-    len = end - start + 1;
-    ptr = malloc(len + 1);
-    if (!ptr)
-        return (NULL);
-    ft_strlcpy(ptr, &s1[start], len + 1);
-    return (ptr);
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && is_char_in_set(s1[start], set))
+		start++;
+	while (end >= start && is_char_in_set(s1[end], set))
+		end--;
+	len = end - start + 1;
+	ptr = malloc(len + 1);
+	if (!ptr)
+		return (NULL);
+	ft_strlcpy(ptr, &s1[start], len + 1);
+	return (ptr);
 }
 
-
-
-int main()
-{
-    
-    printf("%s", ft_strtrim("abcde","abcde"));
-
-}
+// int main()
+// {
+//     printf("%s", ft_strtrim("abcdeab","ab"));
+// }
