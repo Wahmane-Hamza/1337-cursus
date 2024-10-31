c6f7e78d9f1a8e86f7b2c5ceedbe811587eca603
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-31 20:51:04 +0100

    g

--- Libft/ft_putchar_fd.c
+++ Libft/ft_putchar_fd.c
@@ -6,20 +6,14 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:54:05 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/31 13:13:56 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/10/31 20:39:51 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include <unistd.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
-
-// int main()
-// {
-//     int fd = open("test.txt",O_WRONLY);
-//     ft_putchar_fd("hello",fd);
-//     printf("%d hi \e[35mThis text is green\n",fd);
-// }
