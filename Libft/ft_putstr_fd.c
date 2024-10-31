b6b77afb21e3e53a80123c62b12df83788229670
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-10-31 20:51:09 +0100

    g

--- Libft/ft_putstr_fd.c
+++ Libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:14:26 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/31 14:55:18 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/10/31 20:41:06 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,16 @@
 #include "libft.h"
 #include <fcntl.h>
 
-void ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-    {
-        ft_putchar_fd(s[i], fd);
-        i++;
-    }
-}
-
-int main()
-{
-    int fd = open("test.txt", O_WRONLY);
-    ft_putstr_fd("hello",fd);
-    close(fd);
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i])
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
 }
