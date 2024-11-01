56e8dd50f46000b5a6da65c7b1ff56334b576827
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-01 20:55:25 +0100

    vb

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:58:50 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/27 14:01:55 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/01 20:52:05 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,14 @@ void	*ft_memset(void *str, int c, size_t n)
 	i = 0;
 	while (i < n)
 		ptr[i++] = uc;
+	ptr[i] = '\0';
 	return (str);
 }
 
-// int main()
-// {
-// 	// 1337
-// 	int arr = 1;
-// 	//[][][][]
-// 	printf("%d\n", arr);
-// 	// 128 64 32 16 8 4 2 1
-// 	// 0    0  0  0  0 1 0 1
-// 	// 1     1  1  1  1 1 1 1
-// 	ft_memset(&arr, 0, 4);  //260
-// 	ft_memset(&arr, 5, 2);  //260
-// 	ft_memset(&arr, 255, 1);  //260
-// 	printf("%d\n", arr);
-// }
-// int str[10]= {1,2,3,4,5,6};
-// printf("%d\n", str[1]);
-// ft_memset(str,1,sizeof(int)*2);
-// int i = 0;
-// while (i < 6)
-// {
-// printf("%d\n", str[i++]);
-// }
+int main()
+{
+	int s[] = {1337};
+	// printf("%s",ft_memset(s, 35, 7));
+	printf("%c\n" , 255+36);
+}
+
