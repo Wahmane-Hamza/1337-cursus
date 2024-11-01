cdff39d8ede035173f208bb98118bf92d63ca8fe
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-01 20:56:00 +0100

    vcv

--- Libft/ft_lstsize.c
+++ Libft/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 18:52:54 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/01 19:03:14 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/01 19:14:16 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,14 @@ int ft_lstsize(t_list *lst)
         nx = nx->next;
         count++;
     }
+    return (count);
 }
 
 
 int main()
 {
-    
+    t_list  *list = NULL;
+
+    t_list *node1 = ft_lstnew("node 1");
+    t_list *node2 = ft_lstnew("node 2");
 }
