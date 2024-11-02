201baccbaa587f5aae3933b161ee21526c54df2e
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-02 18:07:53 +0100

    sd

--- Libft/ft_putchar_fd.c
+++ Libft/ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:54:05 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/01 20:23:00 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/02 13:57:29 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,8 @@ void	ft_putchar_fd(char c, int fd)
 	write(fd, &c, 1);
 }
 
-
-int main()
-{
-	int	fd = open("test.txt", O_WRONLY);
-	ft_putchar_fd('h',fd);
-}
+// int main()
+// {
+// 	int	fd = open("test.txt", O_WRONLY);
+// 	ft_putchar_fd('h',fd);
+// }
