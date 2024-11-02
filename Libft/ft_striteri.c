8ae973a707a96a510aa0f6b49782ff8e12510dc3
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-02 18:07:56 +0100

    sd

--- Libft/ft_striteri.c
+++ Libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:05:04 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/01 20:16:14 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/02 13:56:59 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,14 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	}
 }
 
-void fe(unsigned int i,char *s)
-{
-    i = 0;
-    *s = *s + 1;
-}
-int main()
-{
-    char s[] = "aabcdefg";
-    ft_striteri(s,fe);
-    printf("%s",s);
-}
+// void fe(unsigned int i,char *s)
+// {
+//     i = 0;
+//     *s = *s + 1;
+// }
+// int main()
+// {
+//     char s[] = "aabcdefg";
+//     ft_striteri(s,fe);
+//     printf("%s",s);
+// }
