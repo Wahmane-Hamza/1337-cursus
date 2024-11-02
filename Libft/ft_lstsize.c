0f57df0c252448f324c6fef77cdbd4596bd56fe9
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-02 18:04:07 +0100

    sd

--- Libft/ft_lstsize.c
+++ Libft/ft_lstsize.c
@@ -6,38 +6,23 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 18:52:54 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/01 21:25:25 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/02 16:19:56 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_lstsize(t_list *lst)
+int	ft_lstsize(t_list *lst)
 {
-    t_list    *nx;
-    int count;
+	t_list	*nx;
+	int		count;
 
-    count = 0;
-    nx = lst;
-    while (nx)
-    {
-        nx = nx->next;
-        count++;
-    }
-    return (count);
-}
-
-
-int main()
-{
-    t_list  *list = NULL;
-
-    t_list *node1 = ft_lstnew("node 1");
-    t_list *node2 = ft_lstnew("node 2");
-    t_list *node3 = ft_lstnew("node 3");
-
-    ft_lstadd_front(&list,node1);
-    ft_lstadd_front(&list,node2);
-    ft_lstadd_front(&list,node3);
-    printf("%d",ft_lstsize(list));
+	count = 0;
+	nx = lst;
+	while (nx)
+	{
+		nx = nx->next;
+		count++;
+	}
+	return (count);
 }
