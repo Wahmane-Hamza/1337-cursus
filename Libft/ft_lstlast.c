d28c89bddb82d284122b962537dac2c0d0b63767
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-03 14:01:32 +0100

    xz

--- Libft/ft_lstlast.c
+++ Libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 14:04:40 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/02 16:24:52 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/03 10:49:21 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,3 @@ t_list	*ft_lstlast(t_list *lst)
 	}
 	return (last);
 }
-
-// int main()
-// {
-//     t_list  *list = NULL;
-
-//     t_list *node1 = ft_lstnew("node 1");
-//     t_list *node2 = ft_lstnew("node 2");
-//     t_list *node3 = ft_lstnew("node 3");
-
-//     ft_lstadd_front(&list,node1);
-//     ft_lstadd_front(&list,node2);
-//     ft_lstadd_front(&list,node3);
-//     printf("%s\n",list->next->next->content);
-//     printf("%s\n",ft_lstlast(list)->content);
-// }
