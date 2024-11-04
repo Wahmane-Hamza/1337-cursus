5c93b9ca7b7c74b917b8a8d6eb11cff6b8fe707e
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 12:15:10 +0100

    add

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:45:12 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:10:44 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 12:12:25 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,23 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
-	size_t	j;
-	size_t	k;
+	size_t	s;
+	size_t	d;
 
-	if (!src || !dst || dstsize == 0)
-		return (0);
-	j = ft_strlen(src);
-	k = ft_strlen(dst);
-	if (dstsize <= k)
-		return (k + j);
+	if (!src || !dst)
+		return (NULL);
+	s = ft_strlen(src);
+	d = ft_strlen(dst);
+	if (dstsize == 0)
+		return (s);
+	if (d >= dstsize)
+		return (dstsize + s);
 	i = 0;
-	while (i + k < dstsize - 1)
+	while ((i + d) < (dstsize - 1) && src[i])
 	{
-		dst[k + i] = src[i];
+		dst[d + i] = src[i];
 		i++;
 	}
-	dst[k + i] = 0;
-	return (k + j);
+	dst[d + i] = '\0';
+	return (d + s);
 }
