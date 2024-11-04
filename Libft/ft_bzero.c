2a285542dca1b06e5cfed5e485919652e19888e2
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 10:14:34 +0100

    sd

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -6,43 +6,13 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 08:25:52 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/27 13:44:44 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:06:42 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
-
-static void	*ft_memset(void *str, int c, size_t n)
-{
-	char			*ptr;
-	unsigned char	uc;
-	size_t			i;
-
-	if (!str)
-		return (NULL);
-	ptr = str;
-	uc = c;
-	i = 0;
-	while (i < n)
-		ptr[i++] = uc;
-	return (str);
-}
+#include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
 }
-
-// int main() {
-//     // Test 1: Standard case
-//     char test1[10] = "123456789";
-//     printf("Before ft_bzero: %s\n", test1);
-//     ft_bzero(test1, 5);
-//     printf("After ft_bzero :");
-//     int i = 0;
-// 	while(i < 10)
-// 	{
-//         printf("%c", test1[i++]);
-// 	}
-//     return 0;
-// }
