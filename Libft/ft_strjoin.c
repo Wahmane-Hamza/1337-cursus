0877307ba1bf8b84492192565228a710c56ab34b
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 12:15:07 +0100

    add

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 15:45:11 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:10:33 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 12:05:32 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,16 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*ptr;
-	int		len;
-	int		i;
+	char		*ptr;
+	size_t		len;
 
 	if (!s1 || !s2)
 		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	ptr = malloc(len + 1);
+	len = (ft_strlen(s1) + ft_strlen(s2)) + 1;
+	ptr = (char *)malloc(len);
 	if (!ptr)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		ptr[i] = s1[i];
-		i++;
-	}
-	ft_strlcat(ptr, s2, len + 1);
+	ft_strlcpy(ptr, s1, len);
+	ft_strlcat(ptr, s2, len);
 	return (ptr);
 }
