30bb14ea1edd813293016bee3758704fb83b8b64
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 12:46:09 +0100

    d

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 12:56:56 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:08:46 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 12:27:08 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
-	char	*dst2;
-	char	*src2;
+	size_t				i;
+	unsigned char		*dst2;
+	const unsigned char	*src2;
 
-	dst2 = (char *)dst;
-	src2 = (char *)src;
-	if (!dst || !src)
+	if (!dst && !src)
 		return (NULL);
+	dst2 = (unsigned char *)dst;
+	src2 = (const unsigned char *)src;
 	i = 0;
 	while (i < n)
 	{
