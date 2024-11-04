52d19cd33383de58a0c61233dfc558465a2258c3
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 13:33:03 +0100

    ad

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 16:18:20 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:08:30 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 13:15:46 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t				i;
 	const unsigned char	*str;
 
-	if (!s)
-		return (NULL);
 	c2 = (unsigned char)c;
 	i = 0;
 	str = (const unsigned char *)s;
