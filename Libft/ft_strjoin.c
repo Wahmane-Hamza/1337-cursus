678c1fdd9521afff07af96aa70e6c4b30f139e71
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 10:16:27 +0100

    add

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,46 +6,11 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 15:45:11 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/27 18:58:30 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:10:33 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-static size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-	size_t	j;
-	size_t	k;
-
-	if (!src || !dst || dstsize == 0)
-		return (0);
-	k = ft_strlen(dst);
-	j = ft_strlen(src);
-	if (dstsize <= k)
-		return (k + j);
-	i = 0;
-	while (i + k < dstsize - 1)
-	{
-		dst[k + i] = src[i];
-		i++;
-	}
-	dst[k + i] = 0;
-	return (k + j);
-}
+#include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -68,10 +33,3 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ft_strlcat(ptr, s2, len + 1);
 	return (ptr);
 }
-
-// int main()
-// {
-//     char str1[] ="aa ";
-//     char str2[] ="";
-//     printf("%s",ft_strjoin(str1,str2));
-// }
