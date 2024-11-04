7834c8771fe696c9944a696068ee08183aeaa909
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 10:15:58 +0100

    add

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,11 +6,11 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 16:18:20 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/27 14:01:38 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:08:30 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
@@ -31,11 +31,3 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
-
-// int main() {
-// 	char str[] = "Hello world";
-// 	char ptr = '\0';
-// 	printf("%s\n",memchr(str,ptr,11));
-// 	printf("%p\n%p\n",&str[11],memchr(str,ptr,11));
-// 	return 0;
-// }
