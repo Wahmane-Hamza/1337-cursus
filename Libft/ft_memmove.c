c49ae21132c12e4600e6a5b86131f3cdd0ad8e41
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 10:16:05 +0100

    add

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,11 +6,11 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 07:26:51 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/27 14:01:52 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:08:54 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
@@ -25,26 +25,3 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		dst2[len] = src2[len];
 	return (dst);
 }
-
-// int main()
-// {
-// 	char p1[10] = "abcdef";
-// 	char *p2 = p1+2;
-
-// 	int i = 0;
-// 	while (i < 4)
-// 	{
-// 		printf("%c",p2[i]);
-// 		i++;
-// 	}
-
-// 	ft_memmove(p2,p1,3);
-// 	printf("\n");
-
-// 	int j = 0;
-// 	while (j < 4)
-// 	{
-// 		printf("%c",p2[j]);
-// 		j++;
-// 	}
-// }
