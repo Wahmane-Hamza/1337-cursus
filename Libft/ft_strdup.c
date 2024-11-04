b6541aca5d6e348a5864530d04d9034c4f622768
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 12:15:05 +0100

    add

--- Libft/ft_strdup.c
+++ Libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 10:41:25 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:10:02 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:47:41 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@ char	*ft_strdup(const char *s1)
 {
 	int		len;
 	char	*ptr;
-	int		i;
 
 	len = ft_strlen(s1);
 	ptr = malloc(len + 1);
 	if (!ptr)
 		return (NULL);
-	i = 0;
 	ft_strlcpy(ptr, s1, len + 1);
 	return (ptr);
 }
