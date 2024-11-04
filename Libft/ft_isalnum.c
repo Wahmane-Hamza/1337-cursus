6bb9d909c3b926f12f602ff2e8a1035fe5836136
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 10:14:42 +0100

    sa

--- Libft/ft_isalnum.c
+++ Libft/ft_isalnum.c
@@ -6,34 +6,13 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:00:55 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/27 14:01:21 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:07:20 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
-
-static int	ft_isalpha(int c)
-{
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
-
-static int	ft_isdigit(int c)
-{
-	return ((c >= '0' && c <= '9'));
-}
+#include "libft.h"
 
 int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
 }
-
-// int main(int argc,char **argv){
-// 	int i = 1;
-// 	while (i < argc)
-// 	{
-// 		int c = ft_isalnum(argv[i][0]) + 48;
-// 		write(1, &c, 1);
-// 		i++;
-// 	}
-//     return 0;
-// }
