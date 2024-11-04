5f17874a0211ac52bc96c99d9557b6b54af1dd4b
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 12:15:13 +0100

    add

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 07:27:43 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:10:53 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 12:14:31 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 	size_t	j;
 
-	if (!src || !dst)
-		return (0);
-	j = 0;
-	while (src[j])
-		j++;
-	if (dstsize == 0)
+	j = ft_strlen(src);
+	if (dstsize == 0 || !dst)
 		return (j);
 	i = 0;
 	while (i < dstsize - 1 && src[i])
