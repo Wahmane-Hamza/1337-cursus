8390295d4a4a584b14211d7c4c777a8f967f1c6c
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 17:59:24 +0100

    df

--- Libft/ft_lstmap_bonus.c
+++ Libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 10:50:22 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 12:17:18 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 17:59:12 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*node;
+	void	*new_coontent;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		node = ft_lstnew(f(lst->content));
+		new_coontent = f(lst->content);
+		node = ft_lstnew(new_coontent);
 		if (!node)
 		{
-			ft_lstclear(&node, del);
+			ft_lstclear(&new_list, del);
+			del(new_coontent);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, node);
