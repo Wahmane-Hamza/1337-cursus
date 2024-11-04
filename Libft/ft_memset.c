4b3343af0ed6abd2b28c3808b1052edbd4214fa9
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 13:33:05 +0100

    add

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:58:50 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:46:10 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 13:14:20 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ void	*ft_memset(void *str, int c, size_t n)
 	unsigned char	uc;
 	size_t			i;
 
-	if (!str)
-		return (NULL);
 	ptr = str;
 	uc = (unsigned char)c;
 	i = 0;
