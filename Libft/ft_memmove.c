03ccccd87270cae4a1d2348fa069e1b032fd9716
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 12:15:00 +0100

    add

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 07:26:51 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:08:54 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 11:26:58 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,28 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*dst2;
-	char	*src2;
+	unsigned char		*dst2;
+	const unsigned char	*src2;
+	size_t				i;
 
-	if (!dst || !src)
+	if (!dst && !src)
 		return (NULL);
-	dst2 = (char *)dst;
-	src2 = (char *)src;
-	while (len--)
-		dst2[len] = src2[len];
+	dst2 = (unsigned char *)dst;
+	src2 = (const unsigned char *)src;
+	i = 0;
+	if (dst2 > src2)
+	{
+		while (i < len)
+		{
+			dst2[len - 1] = src2[len - 1];
+			len--;
+		}
+		return (dst);
+	}
+	while (i < len)
+	{
+		dst2[i] = src2[i];
+		i++;
+	}
 	return (dst);
 }
