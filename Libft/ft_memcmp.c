dbf84b0381930652a5a658b6f3905d8af7e1e20e
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 10:16:00 +0100

    add

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -6,11 +6,11 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 09:06:27 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/27 14:01:41 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:08:36 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
@@ -31,10 +31,3 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
-
-// int main()
-// {
-//     char str1[] = "abcde";
-//     char str2[] = "abcdee";
-//     printf("%d",ft_memcmp(str1,str2,6));
-// }
