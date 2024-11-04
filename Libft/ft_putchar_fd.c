f1abc582b22c5f3d54c16adf90a3bdd99ea5543b
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 10:16:10 +0100

    add

--- Libft/ft_putchar_fd.c
+++ Libft/ft_putchar_fd.c
@@ -6,20 +6,13 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:54:05 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/02 13:57:29 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:09:12 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <fcntl.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
-
-// int main()
-// {
-// 	int	fd = open("test.txt", O_WRONLY);
-// 	ft_putchar_fd('h',fd);
-// }
