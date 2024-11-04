4c3129a9dd3233be68dbbd9250ced232bf9df543
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 10:14:59 +0100

    a

---
+++ Libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 16:28:02 by hwahmane          #+#    #+#             */
+/*   Updated: 2024/11/04 10:07:55 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (*lst || new)
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+}
