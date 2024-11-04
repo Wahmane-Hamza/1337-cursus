8acd95915e4911f2e0d459de94e86eb0c456bbd2
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 18:36:02 +0100

    sd

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 07:22:26 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 18:27:36 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 18:35:50 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,69 +14,70 @@
 
 static int	count_word(char const *s, char c)
 {
-	size_t	i;
-	size_t	count;
+	int	num_strings;
+	int	i;
 
 	i = 0;
-	count = 0;
+	num_strings = 0;
 	while (s[i])
 	{
-		while (s[i] == c)
-			i++;
-		if (s[i] && s[i] != c)
-		{
-			count++;
-			while (s[i] && s[i] != c)
-				i++;
-		}
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			num_strings++;
+		i++;
 	}
-	return (count);
+	return (num_strings);
 }
 
 static char	**free_array(char **array, int j)
 {
-	while (j >= 0)
+	while (j > 0)
 	{
-		free(array[--j]);
+		j--;
+		free(array[j]);
 	}
 	free (array);
 	return (NULL);
 }
 
-static char	**fill_array(char **arr, const char *s, char c)
+static char	**fill_array(char const *s, char c, char **array, int i)
 {
-	int		current;
-	int		start;
-	int		count;
+	int		k;
+	int		j;
+	char	*elem;
 
-	current = 0;
-	count = 0;
-	while (s[current])
+	j = 0;
+	while (s[i])
 	{
-		while (s[current] == c)
-			current++;
-		if (s[current] == '\0')
-			break ;
-		start = current;
-		while (s[current] && s[current] != c)
-			current++;
-		arr[count] = ft_substr(s, start, current - start);
-		if (arr[count] == NULL)
-			return (free_array(arr, count));
-		count++;
+		k = 0;
+		while (s[i] != c && s[i] != '\0')
+		{
+			k++;
+			i++;
+		}
+		if (k > 0)
+		{
+			elem = ft_substr(s, i - k, k);
+			if (!elem)
+				return (free_array(array, j));
+			array[j++] = elem;
+		}
+		if (s[i] != '\0')
+			i++;
 	}
-	arr[count] = NULL;
-	return (arr);
+	array[j] = NULL;
+	return (array);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**array;
+	int		i;
 
 	if (!s)
 		return (NULL);
 	array = (char **)malloc((count_word(s, c) + 1) * sizeof(char *));
 	if (!array)
 		return (NULL);
-	return (fill_array(array, s, c));
+	i = 0;
+	return (fill_array(s, c, array, i));
 }
