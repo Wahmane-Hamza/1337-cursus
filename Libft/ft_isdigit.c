adbd54a88bcdad1e803867cd192003859dadc25a
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 10:14:51 +0100

    ad

--- Libft/ft_isdigit.c
+++ Libft/ft_isdigit.c
@@ -6,11 +6,11 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 09:38:38 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/10/27 14:01:32 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 10:07:33 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libc.h"
+#include "libft.h"
 
 int	ft_isdigit(int c)
 {
