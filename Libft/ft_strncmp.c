c0998bbac68587bf18fdef6148bfb1b43013dd7b
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-04 13:33:11 +0100

    add

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 15:18:47 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:11:04 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/04 13:22:49 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (n > i)
+	if (n == 0)
+		return (0);
+	while ((n - 1) > i && s1[i])
 	{
 		if (s1[i] != s2[i])
 		{
@@ -25,5 +27,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		}
 		i++;
 	}
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
