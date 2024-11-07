8f3c911a1092a9c4f2233ef4f5dedf844f342ac0
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-07 14:13:22 +0100

    ad

--- Libft/ft_striteri.c
+++ Libft/ft_striteri.c
@@ -6,9 +6,11 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:05:04 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:10:07 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/07 13:51:27 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
@@ -24,3 +26,16 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 		}
 	}
 }
+
+// void	f(unsigned int i, char *c)
+// {
+// 	if (i % 2)
+// 		*c = ft_toupper(*c);
+// }
+
+// int main()
+// {
+// 	char s[] = "hello";
+// 	ft_striteri(s, f);
+// 	printf("%s\n", s);
+// }
