cf3ef9ae9c77a98b141c6bcd029e356a9c23f3f0
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-07 14:13:32 +0100

    ad

--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 15:09:05 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/01 20:10:56 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/07 13:36:46 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,3 +33,18 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	ptr[i] = '\0';
 	return (ptr);
 }
+
+// char	f(unsigned int i, char c)
+// {
+// 	if(i % 2)
+// 		return (ft_toupper(c));
+// 	return (c);
+// }
+
+// int main()
+// {
+// 	char	s[] = "hello";
+// 	char	*p = ft_strmapi(s,f);
+// 	printf("%s\n",p);
+// 	free(p);
+// }
