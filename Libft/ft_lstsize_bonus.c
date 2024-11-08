a2721d01c9534e2d05ed2ef836887d1e199fb810
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-08 13:12:52 +0100

    add main

--- Libft/ft_lstsize_bonus.c
+++ Libft/ft_lstsize_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 18:52:54 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/02 16:19:56 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:00:55 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,20 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (count);
 }
+
+// int main()
+// {
+// 	t_list *list = NULL;
+
+// 	t_list *node1 = ft_lstnew(ft_strdup("node1"));
+// 	t_list *node2 = ft_lstnew(ft_strdup("node2"));
+// 	t_list *node3 = ft_lstnew(ft_strdup("node3"));
+// 	t_list *node4 = ft_lstnew(ft_strdup("node4"));
+
+// 	ft_lstadd_back(&list,node1);
+// 	ft_lstadd_back(&list,node2);
+// 	ft_lstadd_back(&list,node3);
+// 	ft_lstadd_back(&list,node4);
+
+// 	printf("%d",ft_lstsize(list));
+// }
