9c06d7c8922b7528b6fb1824e7a84628e45f7389
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-08 13:12:39 +0100

    add main

--- Libft/ft_lstdelone_bonus.c
+++ Libft/ft_lstdelone_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 16:54:05 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/03 09:49:31 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:03:47 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,25 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 	del(lst->content);
 	free(lst);
 }
+
+// void f(void *s)
+// {
+// 	free(s);
+// }
+// int main()
+// {
+// 	t_list *list = NULL;
+// 	t_list *node1 = ft_lstnew(ft_strdup("node1"));
+// 	t_list *node2 = ft_lstnew(ft_strdup("node2"));
+// 	t_list *node3 = ft_lstnew(ft_strdup("node3"));
+// 	t_list *node4 = ft_lstnew(ft_strdup("node4"));
+// 	ft_lstadd_back(&list,node1);
+// 	ft_lstadd_back(&list,node2);
+// 	ft_lstadd_back(&list,node3);
+// 	ft_lstadd_back(&list,node4);
+// 	ft_lstdelone(node4, f);
+// 	printf("%s\n",node1->content);
+// 	printf("%s\n",node2->content);
+// 	printf("%s\n",node3->content);
+// 	printf("%s\n",node4->content);
+// }
