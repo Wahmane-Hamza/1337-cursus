e5b9939faf4116da734d9718d19149897d88e844
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-08 13:12:16 +0100

    add main

--- Libft/ft_isascii.c
+++ Libft/ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:13:06 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:07:31 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/07 18:13:45 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,10 @@ int	ft_isascii(int c)
 {
 	return ((c >= 0 && c <= 127));
 }
+
+// int	main()
+// {
+// 	printf("%d\n",ft_isascii('@'));
+// 	printf("%d\n",ft_isascii('a'));
+// 	printf("%d\n",ft_isascii(255));
+// }
