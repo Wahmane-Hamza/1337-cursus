884396e2382075a42d7a1e6cbf9ee3d877a6efe6
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-08 13:12:18 +0100

    add main

--- Libft/ft_isdigit.c
+++ Libft/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 09:38:38 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:07:33 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:54:43 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,9 @@ int	ft_isdigit(int c)
 {
 	return ((c >= '0' && c <= '9'));
 }
+
+// int	main()
+// {
+// 	printf("%d\n",ft_isdigit('@'));
+// 	printf("%d\n",ft_isdigit('1'));
+// }
