0a8841767fce9b2f936e79e2539b316868302c7e
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-11-08 13:12:10 +0100

    add main

--- Libft/ft_isalnum.c
+++ Libft/ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 10:00:55 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/11/04 10:07:20 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/11/07 17:57:10 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,10 @@ int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
 }
+
+// int	main()
+// {
+// 	printf("%d\n",ft_isalnum('@'));
+// 	printf("%d\n",ft_isalnum('a'));
+// 	printf("%d\n",ft_isalnum('6'));
+// }
