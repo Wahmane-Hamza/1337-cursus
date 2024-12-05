ad4d8c43703bb4d438f123006c106227cb2a2b75
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-05 16:19:17 +0100

    sds

---
+++ Printf/ft_print_str.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_str.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 14:01:32 by hwahmane          #+#    #+#             */
+/*   Updated: 2024/12/05 14:03:49 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_print_str(char *str)
+{
+    int	count;
+
+	count = 0;
+	if (!str)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	while (*str)
+	{
+		count += ft_print_char(*str);
+		str++;
+	}
+	return (count);
+}
