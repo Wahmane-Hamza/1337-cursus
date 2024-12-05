d91c4fcf7975395d270b2c3a19aec090bc773fd8
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-05 16:19:24 +0100

    sd

---
+++ Printf/ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 11:48:19 by hwahmane          #+#    #+#             */
+/*   Updated: 2024/12/05 15:22:14 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <unistd.h>
+
+int ft_printf(const char *format, ...);
+int ft_print_char(int c);
+int	ft_print_str(char *str);
+int	ft_print_nbr(int nbr);
+int	ft_print_unsig_nbr(unsigned int n);
+int	ft_print_hexa(unsigned long nbr, char *base);
+int	ft_print_ptr(unsigned long n);
+
+#endif
