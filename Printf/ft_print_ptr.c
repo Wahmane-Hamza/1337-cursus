529ed5e7016e313c90672d63dcb2b5322904ab1c
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-05 19:29:57 +0100

    dfd

--- Printf/ft_print_ptr.c
+++ Printf/ft_print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 15:08:40 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/12/05 15:21:52 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/12/05 16:39:39 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_print_ptr(unsigned long n)
 {
-    if ((void *)n == NULL)
+	if ((void *)n == NULL)
 		return (write(1, "(nil)", 5));
 	return (write(1, "0x", 2) + ft_print_hexa(n, "0123456789abcdef"));
 }
