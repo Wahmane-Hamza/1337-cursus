6ec7789afd5e7f2b47f48f0a3ffd086d5475fa28
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-10 16:25:10 +0100

    sd

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,32 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 13:17:56 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/12/10 13:17:57 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/12/10 15:39:20 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char    *get_n(char *buffer, int fd)
+{
+    int i;
+
+    i = 0;
+    while (i < BUFFER_SIZE)
+    {
+        
+    }
+    
+}
+
+char *get_next_line(int fd)
+{
+    static char *buffer;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+    if (!buffer)
+        return (NULL);
+    buffer = get_n(buffer , fd);
+}
