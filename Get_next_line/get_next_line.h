201784e2ecf960e88f3f07396ed665e51f7fefae
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-10 16:25:12 +0100

    sd

---
+++ Get_next_line/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 13:35:35 by hwahmane          #+#    #+#             */
+/*   Updated: 2024/12/10 15:29:31 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define    GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+# endif
+
+
+
+#endif
