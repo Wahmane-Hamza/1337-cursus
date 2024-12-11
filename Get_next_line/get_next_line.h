ca11fd42b713357977633093003699ed62a9c85c
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-11 11:48:46 +0100

    as

--- Get_next_line/get_next_line.h
+++ Get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 13:35:35 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/12/10 15:29:31 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/12/11 11:43:14 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 # define    GET_NEXT_LINE_H
 
 # include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
 
 # ifndef BUFFER_SIZE
 # define BUFFER_SIZE 10
 # endif
 
-
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *stack, char *buffer);
 
 #endif
