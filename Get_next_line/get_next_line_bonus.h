46bb7f463f066f21216f11416d8a553af0c6c8ba
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-12 18:31:43 +0100

    ss

--- Get_next_line/get_next_line_bonus.h
+++ Get_next_line/get_next_line_bonus.h
@@ -6,28 +6,28 @@
 /*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 16:14:57 by hwahmane          #+#    #+#             */
-/*   Updated: 2024/12/12 16:25:22 by hwahmane         ###   ########.fr       */
+/*   Updated: 2024/12/12 16:59:01 by hwahmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
-# define    GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
-# include <stdlib.h>
 # include <fcntl.h>
-# include <unistd.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
+#  define BUFFER_SIZE 100000
 # endif
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(char *str);
-int     ft_strchr(const char *s, int c);
+int		ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
-int	    before_n_stack(char *stack);
-int	    after_n_stack(char *stack, int i);
+int		before_n_stack(char *stack);
+int		after_n_stack(char *stack, int i);
 char	*ft_strdup(char *s1);
 
 #endif
