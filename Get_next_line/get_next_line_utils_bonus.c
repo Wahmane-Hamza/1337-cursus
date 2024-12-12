6049b0e5f2c0c8a5a72bf7703b3d1c6e0b5b8cd8
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-12 16:52:07 +0100

    sd

---
+++ Get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwahmane <hwahmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/12 16:15:00 by hwahmane          #+#    #+#             */
+/*   Updated: 2024/12/12 16:16:52 by hwahmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int ft_strchr(const char *s, int c)
+{
+	int				i;
+
+    if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strdup(char *s1)
+{
+	char	*dst;
+	size_t  i;
+
+	dst = (char *)malloc(ft_strlen(s1) + 1);
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*dst;
+	size_t 	i;
+	size_t	size;
+
+	if (s1 && !s2)
+		return (ft_strdup(s1));
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	dst = malloc(size);
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		dst[i++] = *s1++;
+	while (*s2)
+		dst[i++] = *s2++;
+	dst[i] = '\0';
+	return (dst);
+}
+
+int	before_n_stack(char *stack)
+{
+	int	i;
+
+	i = 0;
+	while (stack[i] != '\n' && stack[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	after_n_stack(char *stack, int i)
+{
+	int j;
+
+	j = 0;
+	while (stack[i])
+	{
+		i++;
+		j++;
+	}
+	return (j);
+}
