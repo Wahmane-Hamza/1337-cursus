c65c63f5289540ca14434661c4a9a01ea0257cf4
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-12 16:09:38 +0100

    sd

--- Get_next_line/test.c
+++ Get_next_line/test.c
@@ -1,18 +1,37 @@
 #include "get_next_line.h"
+// int main()
+// {
+
+//     int fd = open("test.txt", O_CREAT | O_RDWR, 0644);
+//     char *str = get_next_line(fd);
+//     printf("one : %s",str);
+//     char *str2 = get_next_line(fd);
+
+//     printf("two :%s",str2);
+//     char *str3 = get_next_line(fd);
+
+//     printf("three :%s",str3);
+
+//     free(str);
+//     free(str2);
+//     free(str3);
+// }
+
+
 int main()
 {
-
-    int fd = open("test.txt", O_CREAT | O_RDWR, 0644);
-    char *str = get_next_line(fd);
-    printf("one : %s",str);
-    char *str2 = get_next_line(fd);
-
-    printf("two :%s",str2);
-    char *str3 = get_next_line(fd);
-
-    printf("three :%s",str3);
-
-    free(str);
-    free(str2);
-    free(str3);
+  printf("BUFFER_SIZE: %d\n", BUFFER_SIZE);
+  int fd = open("./test.txt", O_RDONLY);
+  char *line;
+  free(NULL);
+  line = get_next_line(fd);
+  while (line)
+  {
+    printf("=============\n");
+    printf("line:[%s]", line);
+    printf("=============\n");
+    free(line);
+    line = get_next_line(fd);
+  }
+  return 0;
 }
