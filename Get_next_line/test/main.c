2931e0629ef917eaabdc66171208e7ebdd5d6974
Wahmane-Hamza <hamza.wahm5@gmail.com>
2024-12-13 13:51:42 +0100

    fg

--- Get_next_line/test/main.c
+++ Get_next_line/test/main.c
@@ -1,24 +1,19 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-int main() {
-    // Write C code here
-    char *line;
-    char str[] = "hello";
-    line = malloc(1);
-    line[0] = '\0';
-    printf("line len =>%ld\n",strlen(line));
-    int i = 0;
-    while(str[i])
-    {
-        line[i] = str[i];
-        i++;
-    }
-    line[i] = '\0';
-    printf("%s\n",line);
-    printf("line len =>%ld\n",strlen(line));
-    free(line);
+#include "../get_next_line.h"
 
+int main()
+{
+    printf("BUFFER_SIZE: %d\n", BUFFER_SIZE);
+    int fd = open("test.txt", O_RDONLY);
+    char *line;
+    free(NULL);
+    line = get_next_line(fd);
+    while (line)
+    {
+        printf("===============================================================\n");
+        printf("line:[%s]", line);
+        printf("===============================================================\n");
+        free(line);
+        line = get_next_line(fd);
+    }
     return 0;
 }
